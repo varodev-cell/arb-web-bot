29e020c423781daf83229e7ca954cd94dbb80186
varodev-cell <vlad.shatrov.aro@gmail.com>
2025-10-05 12:59:32 +0300

    Update db.py

--- backend/app/db.py
+++ backend/app/db.py
@@ -3,8 +3,8 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 from .config import settings
 engine = create_async_engine(settings.DATABASE_URL, future=True, echo=False)
 AsyncSessionLocal = sessionmaker(engine, class_=AsyncSession,
-expire_on_commit=False)
+                                 expire_on_commit=False)
 Base = declarative_base()
 async def get_session():
-async with AsyncSessionLocal() as session:
-yield session
+  async with AsyncSessionLocal() as session:
+  yield session
