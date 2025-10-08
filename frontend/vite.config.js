8e479335d2137aef8c2a8f6d09dd5b510f7d98e8
varodev-cell <vlad.shatrov.aro@gmail.com>
2025-10-08 15:39:29 +0300

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
-  import react from '@vitejs/plugin-react'
-    export default defineConfig({
-      plugins: [react()],
-      base: '/arb-web-bot/', // важно для GitHub Pages (если репо: username/arb-
-      web-bot)
-      server: { port: 5173 }
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/arb-web-bot/', // важно для GitHub Pages (username.github.io/arb-web-bot)
+  server: { port: 5173 }
 })
